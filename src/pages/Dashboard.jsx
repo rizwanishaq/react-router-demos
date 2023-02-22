59e0efaa3be3e0d74b0ec8170695c10c6dba3c3a
rizwanishaq <rizwanishaq@gmail.com>
2023-02-22 17:19:34 +0100

    video 9 finished

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,35 +1,62 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import AddBudgetForm from '../components/AddBudgetForm'
 import Intro from '../components/Intro'
 
 // helper functions
-import { fetchData } from '../helpers'
+import { createBudget, fetchData } from '../helpers'
 
 // loader
 export function dashboardLoader(){
     const userName = fetchData("userName")
-    return {userName}
+    const budgets = fetchData("budgets")
+    return {userName, budgets}
 }
 
 // action
 export async function dashboardAction({request}) {
   const data = await request.formData();
-  const formData = Object.fromEntries(data)
-  try {
-    localStorage.setItem("userName", JSON.stringify(formData.userName))
-    return toast.success(`Welcome ${formData.userName}`)
-    
-  } catch (error) {
-      throw new Error("There was a problem creating your account.")
+  const {_action, ...values} = Object.fromEntries(data)
+  if(_action === "newUser") {
+    try {
+      localStorage.setItem("userName", JSON.stringify(values.userName))
+      return toast.success(`Welcome ${values.userName}`)
+      
+    } catch (error) {
+        throw new Error("There was a problem creating your account.")
+    }
+  }
+  if(_action === "createBudget") {
+    try {
+      // create budget
+      createBudget({
+        name: values.newBudget,
+        amount: values.newBudgetAmount
+      })
+
+      return toast.success("Budget created successfully")
+    } catch (error) {
+      throw new Error("There was a problem creating your budget.")
+    }
   }
 }
 
 const Dashboard = () => {
-    const {userName} = useLoaderData()
+    const {userName, budgets} = useLoaderData()
   return (
     <>
-      {userName ?  (<p>{userName}</p>) : <Intro />}
+      {userName ?  (<div className='dashboard'>
+        <h1>Welcome back, <span className='accent'>{userName}</span></h1>
+        <div className='grid-sm'>
+          {/* {budgets ? () : ()} */}
+          <div className='grid-lg'>
+            <div className='flex-lg'>
+              <AddBudgetForm />
+            </div>
+          </div>
+        </div>
+      </div>) : <Intro />}
     </>
   )
 }
